7181c0bcac3f1ad093f3539f4884693b581a667a
edgar martinez <desarrollo_01@dxt.com.mx>
2025-03-24 14:15:35 -0600

    se installo mui, mui icons,tailwind y react router

---
+++ src/pages/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '@styles/App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <p class="text-5xl font-bold underline">
+        ola
+      </p>
+    </>
+  )
+}
+
+export default App
