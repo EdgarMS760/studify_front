94172d61e131a34b325e4fe748c5719f69dbb250
AnElRdJdormir <angelrodriguezjordan900@hotmail.com>
2025-03-30 13:21:47 -0600

    agregue el AuthGate, FormLogin y FormRegister, modifique el LoginLayout y el Router.jsx

---
+++ src/components/organisms/AuthGate.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const AuthGate = () => {
+
+    useState
+    return (
+        <div className='flex justify-center' >
+            <h1 className='text-3xl font-bold underline text-secondary'>Registro</h1>
+        </div>
+    )
+}
+
+export default AuthGate
---
+++ src/components/organisms/FormLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormLogin = () => {
+    return (
+        <div className='flex justify-center' >
+            <h1 className='text-3xl font-bold underline text-secondary'>Registro</h1>
+        </div>
+    )
+}
+
+export default FormLogin
