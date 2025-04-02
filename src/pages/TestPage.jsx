5adc7bc3e3820eea50e2e516f65ea2f46077feb4
Edgar Martinez <edgarsandoval685@gmail.com>
2025-04-02 10:54:10 -0600

    componentes para imagenes pequenas y texto para estandarizar todo

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import TextCardAtom from '../components/atoms/TextCardAtom';
+import MiniImg from '../components/atoms/MiniImg';
 
 const TestPage = () => {
   return (
-    <div>TestPage</div>
+    <>
+      <TextCardAtom text="Texto normal" />
+      <TextCardAtom text="Texto con highlighted" isHighlighted={true} />
+      <TextCardAtom text="Texto rojo " className="text-red-500 underline" />
+      <MiniImg src={"https://placehold.co/600"} alt="Imagen de prueba" />
+      <MiniImg src={"https://placehold.co/600x400"} alt="Imagen de prueba" style='rounded-xl'/>
+    </>
   )
 }
 
