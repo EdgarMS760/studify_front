c0fd97f554a0a6d1160489412cc9429e5c3be804
Edgar Martinez <edgarsandoval685@gmail.com>
2025-04-03 16:47:05 -0600

    agregado componente para renderizar mensaje e input para enviar mensajes

--- src/components/molecules/UserPreview.jsx
+++ src/components/molecules/UserPreview.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Avatar, Box, Typography } from '@mui/material';
+import MiniImg from '../atoms/MiniImg';
+import TextCardAtom from '../atoms/TextCardAtom';
 
-const UserPreview = ({user}) => {
-    if (!user) return null;
+const UserPreview = ({ user }) => {
+  // if (!user) return null;
 
-    const { name, image } = user;
-  
-    return (
-      <Box display="flex" alignItems="center" gap={1}>
-        <Avatar src={image} alt={name} />
-        <Typography variant="body1">{name}</Typography>
-      </Box>
-    );
+  //const { name, image } = user;
+
+  return (
+    <>
+      <MiniImg src="https://placehold.co/600x400" alt="test"/>
+      <TextCardAtom text="usuario" className="text-secondaryHover text-sm"/>
+
+    </>
+  );
 }
 
 export default UserPreview
--- src/pages/FeedGroupPage.jsx
+++ src/pages/FeedGroupPage.jsx
@@ -1,63 +1,92 @@
 import React from "react";
 import SideBarGroup from "../components/organisms/SideBarGroup";
 import HeadBarGroup from "../components/organisms/HeadBarGroup";
-import { useTheme } from "@mui/material";
+import { TextField, useTheme } from "@mui/material";
 import clsx from "clsx";
+import MessagesFeed from "../components/organisms/MessagesFeed";
 
 const mockData = [
     { id: 1, image: "https://placehold.co/600x400", text: "grupo 1" },
     { id: 2, image: "https://placehold.co/600x400", text: "grupo 2" },
     { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
-    { id: 3, image: "https://placehold.co/600x400", text: "grupo 3" },
 ];
-
+const dummyMessages = [
+    {
+        time: "10:30 AM",
+        message: "ewe",
+        user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    },
+    {
+        time: "10:45 AM",
+        message: "q we",
+        user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    },
+    {
+        time: "10:45 AM",
+        message: "q roio",
+        user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    },
+    {
+        time: "10:30 AM",
+        message: "ewe",
+        user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    },
+    {
+        time: "10:45 AM",
+        message: "q we",
+        user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    },
+    // {
+    //     time: "10:45 AM",
+    //     message: "q roio",
+    //     user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    // },
+    // {
+    //     time: "10:30 AM",
+    //     message: "ewe",
+    //     user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    // },
+    // {
+    //     time: "10:45 AM",
+    //     message: "q we",
+    //     user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    // },
+    // {
+    //     time: "10:45 AM",
+    //     message: "q roio",
+    //     user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    // },
+    {
+        time: "11:00 AM",
+        message: "q roio de q",
+        user: { name: "edgar", img: "https://via.placeholder.com/50" }
+    }
+];
 const FeedGroupPage = () => {
     const theme = useTheme();
 
     return (
         <div
             className={clsx(
-                "[@media(min-width:1348px)]:flex flex-row max-h-screen overflow-hidden",
+                "h-screen flex [@media(min-width:1348px)]:flex-row overflow-hidden",
                 theme.palette.mode === "dark" ? "bg-black" : "bg-secondary"
             )}
         >
             {/* este se oculta si pantalla es >= 1348px */}
-            <div className="flex hidden [@media(min-width:1348px)]:block h-full">
+            <div className="hidden [@media(min-width:1348px)]:block h-full">
                 <SideBarGroup items={mockData} />
             </div>
 
-            <div className="">
+            <div className="flex flex-col flex-1 h-full">
                 <HeadBarGroup />
+
+                <div className="flex-1 overflow-y-auto">
+                    <MessagesFeed messages={dummyMessages} />
+                </div>
             </div>
         </div>
     );
 };
 
 export default FeedGroupPage;
+
