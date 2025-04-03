5be948919ebfecd3c3c950a369674f36221c3bac
AnElRdJdormir <angelrodriguezjordan900@hotmail.com>
2025-04-02 19:07:04 -0600

    Agregue el diseño del login y el registro (faltan detalles)

--- src/components/templates/LoginLayout.jsx
+++ src/components/templates/LoginLayout.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React from 'react';
+import { Outlet } from 'react-router';
+
+
 
 const LoginLayout = () => {
   return (
-    <>LoginLayout
+    <>
       <Outlet/>
 
       
