0c019debbdaba79c8183c86a2bd9b580feab8b65
AnElRdJdormir <angelrodriguezjordan900@hotmail.com>
2025-04-07 08:48:14 -0600

    Ya esta el Login, Registro, Reportes y grupos

--- src/components/templates/RegisterLayout.jsx
+++ src/components/templates/RegisterLayout.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import { Box } from '@mui/material';
+import React from 'react';
+import { Outlet } from 'react-router';
 
 const RegisterLayout = () => {
   return (
-    <>RegisterLayout
-      <Outlet/>
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+      }}
+    >
+      <Box
+        sx={{
+          width: 700,
+          backgroundColor: '#ffffff',
+          textAlign: 'center',
+          padding: 4,
+          borderRadius: 2,
+          boxShadow: 3,
+        }}
+      >
+        <img src="/Logo.png" alt="Studify Logo" style={{
+          width: 200,
+    
+          margin: '0 auto 20px',
+    
+          display: 'block',
+          }}
+          />
 
-      
-    </>
-  )
-}
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
 
-export default RegisterLayout
+export default RegisterLayout;
