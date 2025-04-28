9001c4fe7c0fd9d0deab0f8317974c6b1565aff2
Edgar Martinez <edgarsandoval685@gmail.com>
2025-04-27 18:24:49 -0600

    agregada funcionalidad para borrar materiales

--- src/components/molecules/CardMaterial.jsx
+++ src/components/molecules/CardMaterial.jsx
@@ -1,21 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
-import { Box, IconButton } from '@mui/material';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import TextCardAtom from '@components/atoms/TextCardAtom';
 import clsx from 'clsx';
-const CardMaterial = ({ data, isTeacher }) => {
+import { deleteMaterial } from '@services/materialService';
+import { useSnackbar } from '@libs/store/SnackbarContext';
+import { deleteImageByUrl } from '@libs/helpers/firebaseUtils';
+
+const CardMaterial = ({ data, isTeacher, onDelete }) => {
+    const { showSnackbar } = useSnackbar();
+    const [open, setOpen] = useState(false);
     const handleDownload = (url) => {
         const link = document.createElement('a');
         link.href = url;
         link.target = '_blank';
-        link.download = ''; // nombre del archivo (opcional, Firebase suele mandar el nombre correcto)
+        link.download = ''; 
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
+    };
+    const handleConfirm = () => {
+        handleDelete(data._id);
+        setOpen(false);
       };
-      
+    const handleDelete = async () => {
+        try {
+            const response = await deleteMaterial(data._id);
+            if (response.message === "Material eliminado exitosamente") {
+                await deleteImageByUrl(data.archivo);
+
+                onDelete();
+                showSnackbar(response.message, "success");
+            }
+        } catch (err) {
+            console.error("Error al obtener materiales:", err);
+            const message = err.response?.data?.error || "Error al obtener materiales.";
+            showSnackbar(message, "error");
+        }
+    }
     return (
         <Box className={clsx('flex flex-row justify-between items-center rounded-lg shadow-md p-4 w-full h-20'
         )}
@@ -73,19 +97,41 @@ const CardMaterial = ({ data, isTeacher }) => {
                 </IconButton>
 
                 {isTeacher &&
+                    (
+                        <>
+                            <IconButton aria-label="delete" onClick={() => { setOpen(true); }} sx={[
+                                (theme) => ({
+                                    color: theme.vars.palette.secondary.main,
+                                }),
+                                (theme) =>
+                                    theme.applyStyles('dark', {
+                                        color: "white",
+                                    }),
+                            ]}>
+                                <DeleteForeverIcon />
+                            </IconButton>
 
-                    (<IconButton aria-label="delete" sx={[
-                        (theme) => ({
-                            color: theme.vars.palette.secondary.main,
-                        }),
-                        (theme) =>
-                            theme.applyStyles('dark', {
-                                color: "white",
-                            }),
-                    ]}>
-                        <DeleteForeverIcon />
-                    </IconButton>)
-                }
+                            <Dialog
+                                open={open}
+                                onClose={() => setOpen(false)}
+                            >
+                                <DialogTitle>¿Confirmar eliminación?</DialogTitle>
+                                <DialogContent>
+                                    <DialogContentText>
+                                        Esta acción no se puede deshacer. ¿Seguro que quieres eliminarlo?
+                                    </DialogContentText>
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={() => setOpen(false)} color="primary">
+                                        Cancelar
+                                    </Button>
+                                    <Button onClick={handleConfirm} color="error" variant="contained">
+                                        Eliminar
+                                    </Button>
+                                </DialogActions>
+                            </Dialog>
+                        </>
+                    )}
             </div>
         </Box>
     )
